482cebd4dc2707476fe618daa8066933be837afe
Oscar Serrano Yanez <2691820@tcs.com>
2026-01-07 12:30:20 -0600

    Se configura test de vitest y react library test

---
+++ src/GifsApp.test.tsx
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import { GifsApp } from "./GifsApp";
+
+describe('GifsApp', () => {
+
+    test('should render component', () => {
+        const { container } = render(<GifsApp />)
+
+        expect(container).toMatchSnapshot();
+    })
+})
